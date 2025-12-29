78366160c4c213221e1da455e8737b017572f3f5
Harshal Bhati <harshalbhati64@gmail.com>
2025-12-29 15:45:39 +0530

    Adding 3 numbers

--- src/calculator.py
+++ src/calculator.py
@@ -2,7 +2,7 @@ import logging
 
 logger = logging.getLogger(__name__)
 
-def add(a: int, b: int) -> int:
-    result = a + b
+def add(a: int, b: int, c: int) -> int:
+    result = a + b + c
     logger.info("Adding %s + %s = %s", a, b, result)
     return result
